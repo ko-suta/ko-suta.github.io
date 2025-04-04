41dfcae0d750d0c2f4038e06a6defcada81a42eb
kostya <k.ovsiannikov@gmail.com>
2025-04-04 15:32:42 +0900

    oprimize files

--- build-site.el
+++ build-site.el
@@ -25,9 +25,6 @@
 (use-package htmlize
   :ensure t)
 
-(use-package ox-tufte
-  :ensure t)
-
 ;; Customize the HTML output
 (setq org-html-validation-link nil            ;; Don't show validation link
       org-html-head-include-scripts nil       ;; Use our own scripts
